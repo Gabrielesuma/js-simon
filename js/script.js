431e6f7b5f2d8ea5f12c6f255211293caa03dda3
Gabrielesuma <gabrielesuma139@gmail.com>
2024-03-20 17:45:59 +0100

    input

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
+let arrayNum = [];
+let newInputs;
+const button = document.getElementById('button');
+
 generateFiveUnique();
 
+setTimeout(genInput, 5000);
+
+button.addEventListener('click', function(){
+    equal(newInputs);
+});
+
+
+
+function equal(newInputs){
+    let arrayEqual = [];
+    if(arrayNum.includes(newInputs.innerHTML)){
+        arrayEqual.push(newInputs.innerHTML);
+    }
+
+    return arrayEqual;
+}
 
 
 function generateFiveUnique(){
-    let arrayNum = [];
 
     while(arrayNum.length < 5){
         let numRandom = getRandomInt(1, 100);
@@ -17,11 +36,23 @@ function generateFiveUnique(){
 }
 
 function genBox(num){
-    const cont = document.querySelector('.container');
+    const cont = document.getElementById('boxes');
     const newBox = document.createElement('div');
     newBox.classList.add('border', 'w-25', 'p-2', 'm-2', 'text-center');
     newBox.innerHTML = num;
     cont.appendChild(newBox);
+}
+
+function genInput(cont2){
+    const cont = document.getElementById('boxes');
+    cont.classList.add('d-none');
+    for(i = 0; i < 5; i++){
+        const cont2 = document.getElementById('inputs');
+        newInputs = document.createElement('input');
+        newInputs.classList.add('form-control', 'm-2');
+        cont2.appendChild(newInputs);
+    }
+    return cont2;
 }
 
 function getRandomInt(min, max){
